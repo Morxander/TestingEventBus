6b8d71d496b84468cd2471d7d03e0210a3df3813
Morad Edwar <me@morad-edwar.com>
2017-03-09 10:15:43 +0200

    Add the MessageEvent class

---
+++ app/src/main/java/morxander/testingeventbus/MessageEvent.java
@@ -0,0 +1,17 @@
+package morxander.testingeventbus;
+
+/**
+ * Created by morxander on 3/9/17.
+ */
+
+public class MessageEvent {
+    private String message;
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
