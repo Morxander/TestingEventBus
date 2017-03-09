d043c47e8013a309d99a955986d4de22fc968078
Morad Edwar <me@morad-edwar.com>
2017-03-09 10:20:19 +0200

    Send the message to EventBus from ChildActivity

--- app/src/main/java/morxander/testingeventbus/ChildActivity.java
+++ app/src/main/java/morxander/testingeventbus/ChildActivity.java
@@ -2,8 +2,11 @@ package morxander.testingeventbus;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+
+import org.greenrobot.eventbus.EventBus;
 
 public class ChildActivity extends AppCompatActivity {
 
@@ -17,5 +20,15 @@ public class ChildActivity extends AppCompatActivity {
         // init the views
         childActivityEditText = (EditText) findViewById(R.id.childactivity_edittext);
         childActivityButton = (Button) findViewById(R.id.childactivity_button);
+        // set the onclick
+        childActivityButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                MessageEvent messageEvent = new MessageEvent();
+                messageEvent.setMessage(childActivityEditText.getText().toString());
+
+                EventBus.getDefault().post(messageEvent);
+            }
+        });
     }
 }
