69a280d41c0ced5a124616473c32a20c79ea7f22
Morad Edwar <me@morad-edwar.com>
2017-03-09 10:18:57 +0200

    Register the MainActivity to the EventBus

--- app/src/main/java/morxander/testingeventbus/MainActivity.java
+++ app/src/main/java/morxander/testingeventbus/MainActivity.java
@@ -1,9 +1,15 @@
 package morxander.testingeventbus;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+
+import org.greenrobot.eventbus.EventBus;
+import org.greenrobot.eventbus.Subscribe;
+import org.greenrobot.eventbus.ThreadMode;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,5 +23,19 @@ public class MainActivity extends AppCompatActivity {
         // init the views
         mainActivityTextView = (TextView) findViewById(R.id.mainactivity_textview);
         mainActivityButton = (Button) findViewById(R.id.mainactivity_button);
+        // set the onclick
+        mainActivityButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getBaseContext(), ChildActivity.class));
+            }
+        });
+        // register to the event bus
+        EventBus.getDefault().register(this);
+    }
+
+    @Subscribe(threadMode = ThreadMode.MAIN)
+    public void onMessageEvent(MessageEvent messageEvent){
+        mainActivityTextView.setText(messageEvent.getMessage());
     }
 }
