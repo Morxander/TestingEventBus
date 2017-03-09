7511f72bd57babc802887cd5ccc3ee9a8dcda690
Morad Edwar <me@morad-edwar.com>
2017-03-09 10:11:15 +0200

    Set the MainActivity views

--- app/src/main/java/morxander/testingeventbus/MainActivity.java
+++ app/src/main/java/morxander/testingeventbus/MainActivity.java
@@ -1,13 +1,21 @@
 package morxander.testingeventbus;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView mainActivityTextView;
+    Button mainActivityButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // init the views
+        mainActivityTextView = (TextView) findViewById(R.id.mainactivity_textview);
+        mainActivityButton = (Button) findViewById(R.id.mainactivity_button);
     }
 }
